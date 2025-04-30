77156ed11a37ac96a52e2b48bbd71388ba6128a2
Bertodo Neto <100353344+Bertodo015@users.noreply.github.com>
2025-04-29 21:55:18 -0400

    Delete backend/dist directory

--- backend/dist/src/matriculas/matriculas.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class MatriculasService {
-    create(data: any): Promise<any>;
-    findAll(): Promise<any>;
-    findOne(fk_disciplinas_id: number, fk_estudantes_fk_usuarios_id: number): Promise<any>;
-    update(fk_disciplinas_id: number, fk_estudantes_fk_usuarios_id: number, data: any): Promise<any>;
-    remove(fk_disciplinas_id: number, fk_estudantes_fk_usuarios_id: number): Promise<any>;
-}
